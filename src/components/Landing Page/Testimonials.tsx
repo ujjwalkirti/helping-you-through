3edea4af06c109f3226b7e9347f0c99ee686a0c6
yashbaldev <113610963+yashbaldev@users.noreply.github.com>
2023-06-13 12:54:25 +0530

    Revert "chore : update what we do"

--- src/components/Landing Page/Testimonials.tsx
+++ src/components/Landing Page/Testimonials.tsx
@@ -1,7 +1,68 @@
+import { poppins } from "@/utils/Fonts";
+import { TestimonialData, responsive } from "@/utils/dummy-data";
+import { useTheme } from "next-themes";
+import Image from "next/image";
 import React from "react";
+import Carousel from "react-multi-carousel";
+
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const { theme } = useTheme();
+
+  const testimonials = TestimonialData.map((el) => (
+    <div key={el.id} className=" bg-white dark:bg-[#20B15A]  p-3 rounded-2xl shadow-md drop-shadow-lg h-full flex flex-col justify-between text-black dark:text-[white] ">
+      <div>
+        <p className="px-7 text-sm font-medium">{el.review}</p>
+      </div>
+      <div className="flex pl-7 mt-2 justify-start ">
+        <Image
+           width= {47} 
+           height= {47} 
+          alt="Remy Sharp"
+          src="" //{el.imageurl}
+           // edher next.config.js me change karne kah raha 
+          className="mr-2 rounded-full"
+        />
+        <div className=" font-semibold ">
+          <p>{el.name}</p>
+          <p className=" text-sm ">{el.AdmissionNo}</p>
+        </div>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="bg-white dark:bg-[#000000] pl-12 pt-10  ">
+      <div className=" two ">
+        <div>
+          <p className={" text-xl font-semibold text-[#20B15A] one " + poppins.className
+    }>
+            {" "}
+            TESTIMONIALS
+          </p>
+          <p className=" font-bold  one text-black dark:text-white ">
+            See what our Almuni say about college
+          </p>
+        </div>
+      </div>
+      <div className="gd-carousel-wrapper">
+        <Carousel
+          itemClass="react-multi-carousel-item "
+          partialVisible={false}
+          responsive={responsive}
+          centerMode={true}
+          swipeable={true}
+          draggable={true}
+          arrows={true}
+          keyBoardControl={true}
+          removeArrowOnDeviceType={["mobile", ]}
+          className="gd-carousel"
+        >
+          {testimonials}
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import Testimonials from "@/components/Landing Page/Testimonials";
 import Loader from "@/components/Loader";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
@@ -8,11 +9,17 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
+
+
+
+
 export default function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
+  const [mounted, setMounted] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
+    setMounted(true);
     const handleStart = () => {
       setLoading(true);
     };
