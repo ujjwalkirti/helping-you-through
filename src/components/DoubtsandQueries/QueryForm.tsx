792c157587a8f6a4d49118be6235aeea833cbbca
Ujjwal Kirti <64329707+ujjwalkirti@users.noreply.github.com>
2023-06-26 23:43:02 +0530

    Chore: created new components and developed UI framework for doubts-and-queries page

---
+++ src/components/DoubtsandQueries/QueryForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QueryForm = () => {
+  return <div>QueryForm</div>;
+};
+
+export default QueryForm;
--- src/pages/doubts-and-queries.tsx
+++ src/pages/doubts-and-queries.tsx
@@ -1,7 +1,18 @@
+import QueryForm from "@/components/DoubtsandQueries/QueryForm";
 import React from "react";
 
 function DoubtsAndQueries() {
-  return <div>DoubtsAndQueries</div>;
+  return (
+    <section>
+      <div>
+        <p>Queries and Doubts</p>
+      </div>
+      <div>
+        {/* Image */}
+        <QueryForm />
+      </div>
+    </section>
+  );
 }
 
 export default DoubtsAndQueries;
