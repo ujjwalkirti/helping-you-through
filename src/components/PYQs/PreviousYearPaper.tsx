141c2a5b892120f24c78c4cad478cdb8f4ad9f70
Sunny <shanigupta120103@gmail.com>
2023-07-05 00:47:08 +0530

    Bug Fix :Fixed design issue in PYQ

--- src/components/PYQs/PreviousYearPaper.tsx
+++ src/components/PYQs/PreviousYearPaper.tsx
@@ -68,31 +68,37 @@ function PreviousYearPaper() {
           <div className="flex flex-col">
             <form>
               <div className="flex  flex-wrap gap-y-5 flex-col changeformtag">
+          
                 <select
                   name="department"
                   onChange={(e) => setDepId(parseInt(e.target.value))}
-                  className="w-2/3  p-3  border-[2px] border-black rounded-md  bg-white text-black"
+                  className="w-2/3  p-3  border-[2px] border-black rounded-md  bg-[#D9D9D9] text-black"
                 >
+                  <option value="" disabled selected hidden>Branch</option>
                   {Departments.map((department) => (
                     <option value={department.id} key={department.id}>
                       {department.name}
                     </option>
                   ))}
                 </select>
+               
                 <select
                   name="semester"
-                  className=" w-1/2 sm:w-1/3 p-3   border-black   border-[2px] rounded-md bg-white text-black"
+                  className=" w-1/2 sm:w-1/3 p-3   border-black   border-[2px] rounded-md bg-[#D9D9D9] text-black"
                   onChange={(e) => setSem(parseInt(e.target.value))}
                 >
+                  <option value="" disabled selected hidden>Semester</option>
                   {Departments[depId - 1].sem.map((semester) => (
                     <option key={semester.id}>{semester.id}</option>
                   ))}
                 </select>
+              
                 <select
                   name="subject"
-                  className=" w-1/3 sm:w-1/4 p-3   border-black   border-[2px] rounded-md  bg-white text-black"
+                  className=" w-1/3 sm:w-1/4 p-3   border-black   border-[2px] rounded-md  bg-[#D9D9D9] text-black"
                   onChange={(e) => setSub(e.target.value)}
                 >
+                  <option value="" disabled selected hidden>Subject</option>
                   {Departments[depId - 1].sem[sem - 1].sub.map((subjects) => (
                     <option
                       key={Departments[depId - 1].sem[sem - 1].sub.indexOf(
