83b81c7de24561e0dedb29b6e07e997b3becb7bc
Yash Baldev <yash8866377591@gmail.com>
2023-07-16 00:46:42 +0530

    Chore:Add Reviews and Reviews Page

--- src/components/Reviews/ReadReviewOptions.tsx
+++ src/components/Reviews/ReadReviewOptions.tsx
@@ -1,7 +1,68 @@
 import React from "react";
+import Image from "next/image";
+import { poppins } from "@/utils/Fonts";
 
 function ReadReviewOptions() {
-  return <div>ReadReviewOptions</div>;
+  return (<div>
+    <div className=" w-10/12 md:w-full  m-auto mb-5 flex max-w-2xl max-h-96 bg-lgreen dark:bg-dgreen text-black dark:text-white rounded-xl shadow shadow-[#00000040] ">
+      <div className=" m-4 w-3 h-80 flex-shrink-0 border-2 border-solid border-black bg-[ #D9D9D9] dark:bg-[#979798]">
+        <div className=" mt-16 w-2.5 h-14 flex-shrink-0 bg-black dark:bg-white"></div>
+      </div>
+      <div>
+        <div className={" w-10/12 md:w-full  mx-16 justify-center flex max-w-32 h-14 flex-col flex-shrink-0   font-semibold "+poppins.className}>Name</div>
+        <div >
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Akriti</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">ACM</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">CHRD</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">CES</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">GDSC</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Drishti</div>
+        </div>
+      </div>
+      <div className=" max-w-0 md:max-w-full flex w-96 h-14 flex-shrink-0 flex-col ">
+        <div className={" hidden md:block  mx-16 mt-4 justify-center text-center font-semibold "+poppins.className}>Student Chapters/clubs</div>
+        <div className=" mx-10 mt-4 justify-end" >
+          <Image className=" flex-shrink-0 border-2 border-solid" src="/assests/student_chapter.png" width={300} height={296} alt="Student Chapters" />
+        </div>
+      </div>
+      
+    </div>
+    <div className=" w-10/12 md:w-full m-auto mb-5 flex max-w-2xl max-h-96 bg-lgreen dark:bg-dgreen text-black dark:text-white rounded-xl shadow shadow-[#00000040] ">
+    <div className=" hidden md:block  w-96 h-14 flex-shrink-0 flex-col ">
+    <div className={" hidden md:block mx-16 mt-4 justify-center text-center font-semibold "+poppins.className}>About Hostels</div>
+        <div className=" mx-10 mt-4 justify-end" >
+        <Image className="  flex-shrink-0 border-2 border-solid" src="/assests/hostels.png" width={300} height={296} alt="Hostels" /> 
+        </div>
+        </div>
+      <div>
+      <div className={" w-10/12 md:w-full  mx-16 justify-center flex max-w-32 h-14 flex-col flex-shrink-0   font-semibold "+poppins.className}>Name</div>
+        <div >
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Gajjar Bhavan</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Mother Terasa Bhavan</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Nehru Bhavan</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Tagore Bhavan</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Sarabhai Bhavan</div>
+          <div className=" text-center mx-2 mb-2 max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Swami Vivekanand Bhavan</div>
+        </div>
+      </div>
+      <div className=" m-4 w-3 h-80 flex-shrink-0 border-2 border-solid border-black bg-[ #D9D9D9] dark:bg-[#979798]">
+        <div className=" mt-16 w-2.5 h-14 flex-shrink-0 bg-black dark:bg-white"></div>
+      </div>  
+    </div>
+    
+    
+
+        
+    
+    </div>
+
+);
 }
 
 export default ReadReviewOptions;
+// Bhabha Bhavan</div>
+//           <div className=" max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Gajjar Bhavan</div>
+//           <div className=" max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Mother Terasa Bhavan</div>
+//           <div className=" max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Swami Vivekanand Bhavan</div>
+//           <div className=" max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Nehru Bhavan</div>
+//           <div className=" max-w-xl h-10 flex-shrink-0 bg-[#D9D9D9] dark:bg-[#979798]">Tagore Bhavan</div>
--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -2,6 +2,7 @@ import GiveReviewBox from "@/components/Reviews/GiveReviewBox";
 import ReadReviewOptions from "@/components/Reviews/ReadReviewOptions";
 import React, { useState } from "react";
 import { Button, ButtonGroup } from "@mui/material";
+import { poppins } from "@/utils/Fonts";
 const Reviews = () => {
   const [choice, setChoice] = useState("read");
   const handleclick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -10,13 +11,20 @@ const Reviews = () => {
   };
   return (
     <section>
-      <p className="text-center m-2">Reviews</p>
-      {/* Option to whether read or give review */}
-      <div className="text-center">
-        <ButtonGroup size="large" className="bg-dgreen" variant="contained">
+      <div className=" mt-3 mb-2 text-center">
+        <svg className="  inline-block w-12 h-16 flex-shrink-0 " width="70" height="80" viewBox="0 0 70 80" fill="none">
+<path d="M39.375 67.0029H56.875V72.157H39.375V67.0029Z" fill="black"/>
+<path d="M39.375 56.6943H65.625V61.8484H39.375V56.6943Z" fill="black"/>
+<path d="M39.375 46.3867H65.625V51.5408H39.375V46.3867Z" fill="black"/>
+<path d="M44.9509 28.9067L35 5.1543L25.0491 28.9067L2.79999 32.7156L18.9 51.2058L15.0981 77.311L30.625 67.6961V61.8721L20.9103 67.8894L23.2116 52.0768L23.6009 49.4044L21.9537 47.5155L12.2019 36.3131L25.6791 34.0067L27.9541 33.6176L28.9734 31.1874L35 16.7999L41.0265 31.1874L42.0459 33.6176L44.3209 34.0067L60.6265 36.8028L61.25 31.6977L44.9509 28.9067Z" fill="black"/>
+</svg>
+      <p className={" inline-block text-center text-2xl font-semibold "+poppins.className}>Reviews</p>
+      </div>{/* Option to whether read or give review */}
+      <div className="text-center text-black dark:text-white mb-4">
+        <ButtonGroup size="small" className="bg-[#00000040]  dark:bg-[#979798]" variant="contained">
           <Button
             id="1"
-            className={`${choice === "read" ? "bg-orange" : null}`}
+            className={` rounded-md text-black dark:text-white ${choice === "read" ? "bg-[#D7F5DC] dark:bg-[#67CE67]" : null}`}
             color="warning"
             onClick={handleclick}
           >
@@ -24,7 +32,7 @@ const Reviews = () => {
           </Button>
           <Button
             id="2"
-            className={`${choice === "give" ? "bg-orange" : null}`}
+            className={` text-black dark:text-white ${choice === "give" ? "bg-[#D7F5DC] dark:bg-[#67CE67]" : null}`}
             color="warning"
             onClick={handleclick}
           >
