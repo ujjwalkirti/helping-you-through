cb0bc549f4fad4cbee80b0e94c8902d005eb619b
Anandsouresh <anandsouresh@gmail.com>
2023-08-16 01:16:06 +0530

    BugFix : "Rectified styling of marketplace page"

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -4,7 +4,8 @@ import VehicleItemsCarousel from "@/components/Marketplace/VehicleItemsCarousel"
 import axios from "axios";
 import Head from "next/head";
 import React, { useEffect, useState } from "react";
-import {IMarketPlace} from '@/utils/Models'
+import { IMarketPlace } from '@/utils/Models'
+import { Button, ButtonGroup } from "@mui/material";
 
 const MarketPlace = () => {
   const [choice, setChoice] = React.useState("details");
@@ -23,13 +24,34 @@ const MarketPlace = () => {
       <Head>
         <title>MarketPlace - Cost Effective solution for all your needs!</title>
       </Head>
-      <p>MarketPlace</p>
-      <div>
-        <p>Product</p>
-        <div>
-          <button onClick={() => setChoice("details")}>Details</button>
-          <button onClick={() => setChoice("add-products")}>Add</button>
-        </div>
+      <div className="flex flex-row justify-center text-center ">
+        <img
+          src="assests\cart.png"
+          alt="Placeholder"
+          className="w-10" />
+        <p className=" mx-3 text-center text-2xl font-bold"> MarketPlace </p>
+      </div>
+
+      <p className="justify-centre mt-4 text-center text-xl font-semibold">Product</p>
+      {/* <div className="justify-items-center"> */}'
+      <div >
+        <ButtonGroup
+          disableElevation
+          variant="contained"
+          aria-label="Disabled elevation buttons"
+          className="justify-center text-center flex "
+        >
+          <Button className={` p-2 ${choice === "details" ? "bg-[#b4f5ce] dark:bg-[#20B15A]" : "bg-[#706b6a]"
+            } hover:bg-[#74da9d] focus:bg-[#b4f5ce] rounded-lg`}
+            onClick={() => setChoice("details")}
+          >
+            Details</Button>
+          <Button className={` p-2 ${choice === "details" ? "bg-[#706b6a]" : "bg-[#b4f5ce] dark:bg-[#20B15A]"
+            } hover:bg-[#74da9d] focus:bg-[#b4f5ce] rounded-lg`}
+            onClick={() => setChoice("add-products")}
+          >
+            Add</Button>
+        </ButtonGroup>
       </div>
       <div>
         {choice === "details" ? (
