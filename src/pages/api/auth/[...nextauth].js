6833f6a505c46c3302bf5915b5d6973224665d81
Ujjwal Kirti <kirti.chargers@gmail.com>
2023-08-24 12:33:37 +0530

    Bug fix: added Next auth secret for production deployment

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,6 +9,7 @@ export const authOptions = {
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 };
 
 export default NextAuth(authOptions);
